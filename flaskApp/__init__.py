df0cd93e97bcb1989d00607688720dca7eff5762
kaw393939 <keith@webizly.com>
2022-03-02 13:34:07 -0500

    flask bootstrap upgrade

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import render_template
 from flaskApp import db, auth, blog, simple_pages
 from flaskApp.context_processors import utility_text_processors
+from flask_bootstrap import Bootstrap5
+
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
@@ -35,6 +37,7 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
     app.register_blueprint(simple_pages.bp)
+    bootstrap = Bootstrap5(app)
 
 
     # make url_for('index') == url_for('blog.index')
